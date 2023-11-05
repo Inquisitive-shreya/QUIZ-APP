e59f3e42ee25dcec18f1dbbc8e68203cde5cdb07
inquisitive-shreya <shreyaaasingh2301@gmail.com>
2023-11-05 16:32:25 +0530

    Finished my Quiz App

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "What is Birth place of lord Ram?",
+            "which animal is our national bird?",
+            "which one is not the programming language?"
+    };
+
+    public static String choices[][] ={
+            {"Dwarika","Ayodhya","Kashi","Ujjain"},
+            {"crow","peahen","peacock","sparrow"},
+            {"php","java","python","notepad"}
+    };
+
+    public static String CorrectAnswer[] ={
+            "Ayodhya",
+            "peacock",
+            "notepad",
+
+
+    };
+
+}
